debecaa2d7fae6e586555a587a77335d14bb0ab4
Dread9182 <84605991+Dread9182@users.noreply.github.com>
2024-09-12 00:37:21 -0500

    Feat: added launcher icons, font and changed files to a very simple app

---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Aula Abierta',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.w500
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple.shade400,
+        elevation: 0.0,
+      ),
+    );
+  }
+}
